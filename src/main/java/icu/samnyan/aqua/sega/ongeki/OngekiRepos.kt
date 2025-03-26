fdfdf66fa3ab9bd9e41ee9919665a7a45d0d97da
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-03-26 17:33:46 -0400

    [O] Ongeki: Remove unused repo functions

--- src/main/java/icu/samnyan/aqua/sega/ongeki/OngekiRepos.kt
+++ src/main/java/icu/samnyan/aqua/sega/ongeki/OngekiRepos.kt
@@ -22,9 +22,6 @@ interface UserActivityRepository : JpaRepository<UserActivity, Long> {
     fun findByUserAndKindAndActivityId(userData: UserData, kind: Int, activityId: Int): Optional<UserActivity>
 
     fun findByUser_Card_ExtIdAndKindOrderBySortNumberDesc(userId: Long, kind: Int): List<UserActivity>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserBossRepository")
@@ -32,23 +29,15 @@ interface UserBossRepository : JpaRepository<UserBoss, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserBoss>
 
     fun findByUserAndMusicId(user: UserData, musicId: Int): Optional<UserBoss>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserCardRepository")
 interface UserCardRepository : JpaRepository<UserCard, Long> {
     fun findByUserAndCardId(userData: UserData, cardId: Int): Optional<UserCard>
 
-    fun findByUser_Card_ExtIdAndCardId(userId: Long, cardId: Int): Optional<UserCard>
-
     fun findByUser_Card_ExtId(userId: Long): List<UserCard>
 
     fun findByUser_Card_ExtId(userId: Long, page: Pageable): Page<UserCard>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserChapterRepository")
@@ -56,9 +45,6 @@ interface UserChapterRepository : JpaRepository<UserChapter, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserChapter>
 
     fun findByUserAndChapterId(userData: UserData, chapterId: Int): Optional<UserChapter>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserCharacterRepository")
@@ -68,17 +54,11 @@ interface UserCharacterRepository : JpaRepository<UserCharacter, Long> {
     fun findByUser_Card_ExtId(userId: Long, page: Pageable): Page<UserCharacter>
 
     fun findByUserAndCharacterId(userData: UserData, characterId: Int): Optional<UserCharacter>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserDataRepository")
 interface UserDataRepository : GenericUserDataRepo<UserData> {
     fun findByCard_ExtIdIn(userIds: Collection<Long>): List<UserData>
-
-    @Transactional
-    fun deleteByCard(card: Card)
 }
 
 @Repository("OngekiUserDeckRepository")
@@ -86,9 +66,6 @@ interface UserDeckRepository : JpaRepository<UserDeck, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserDeck>
 
     fun findByUserAndDeckId(userData: UserData, deckId: Int): Optional<UserDeck>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserEventMusicRepository")
@@ -101,9 +78,6 @@ interface UserEventMusicRepository : JpaRepository<UserEventMusic, Long> {
         type: Int,
         musicId: Int
     ): Optional<UserEventMusic>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserEventPointRepository")
@@ -111,9 +85,6 @@ interface UserEventPointRepository : JpaRepository<UserEventPoint, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserEventPoint>
 
     fun findByUserAndEventId(userData: UserData, eventId: Int): Optional<UserEventPoint>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 
     //@Query(value = "SELECT rank from (SELECT user_id , DENSE_RANK() OVER (ORDER BY point DESC) as rank from ongeki_user_event_point where event_id = :eventId) where user_id == :userId limit 1", nativeQuery = true)
     @Query("SELECT COUNT(u)+1 FROM OngekiUserEventPoint u WHERE u.eventId = :eventId AND u.point > (SELECT u2.point FROM OngekiUserEventPoint u2 WHERE u2.user.id = :userId AND u2.eventId = :eventId)")
@@ -127,9 +98,6 @@ interface UserGeneralDataRepository : JpaRepository<UserGeneralData, Long> {
     fun findByUserAndPropertyKey(user: UserData, key: String): Optional<UserGeneralData>
 
     fun findByUser_Card_ExtIdAndPropertyKey(userId: Long, key: String): Optional<UserGeneralData>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserItemRepository")
@@ -141,9 +109,6 @@ interface UserItemRepository : JpaRepository<UserItem, Long> {
     fun findByUserAndItemKindAndItemId(userData: UserData, itemKind: Int, itemId: Int): Optional<UserItem>
 
     fun findByUser_Card_ExtIdAndItemKind(userId: Long, kind: Int, page: Pageable): Page<UserItem>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserKopRepository")
@@ -151,18 +116,12 @@ interface UserKopRepository : JpaRepository<UserKop, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserKop>
 
     fun findByUserAndKopIdAndAreaId(userData: UserData, kopId: Int, areaId: Int): Optional<UserKop>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 interface UserLoginBonusRepository : JpaRepository<UserLoginBonus, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserLoginBonus>
 
     fun findByUserAndBonusId(userData: UserData, bonusId: Int): Optional<UserLoginBonus>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserMemoryChapterRepository")
@@ -170,9 +129,6 @@ interface UserMemoryChapterRepository : JpaRepository<UserMemoryChapter, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserMemoryChapter>
 
     fun findByUserAndChapterId(userData: UserData, chapterId: Int): Optional<UserMemoryChapter>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserMissionPointRepository")
@@ -180,9 +136,6 @@ interface UserMissionPointRepository : JpaRepository<UserMissionPoint, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserMissionPoint>
 
     fun findByUserAndEventId(userData: UserData, eventId: Int): Optional<UserMissionPoint>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserMusicDetailRepository")
@@ -192,12 +145,7 @@ interface UserMusicDetailRepository : JpaRepository<UserMusicDetail, Long>,
 
     fun findByUser_Card_ExtId(userId: Long, page: Pageable): Page<UserMusicDetail>
 
-    fun findByUser_Card_ExtIdAndMusicId(userId: Long, id: Int): List<UserMusicDetail>
-
     fun findByUserAndMusicIdAndLevel(userData: UserData, musicId: Int, level: Int): Optional<UserMusicDetail>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserMusicItemRepository")
@@ -207,9 +155,6 @@ interface UserMusicItemRepository : JpaRepository<UserMusicItem, Long> {
     fun findByUser_Card_ExtId(userId: Long, page: Pageable): Page<UserMusicItem>
 
     fun findByUserAndMusicId(userData: UserData, musicId: Int): Optional<UserMusicItem>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserOptionRepository")
@@ -217,9 +162,6 @@ interface UserOptionRepository : JpaRepository<UserOption, Long> {
     fun findByUser(userData: UserData): Optional<UserOption>
 
     fun findByUser_Card_ExtId(userId: Long): Optional<UserOption>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserPlaylogRepository")
@@ -227,22 +169,11 @@ interface UserPlaylogRepository : GenericPlaylogRepo<UserPlaylog> {
     fun findByUser_Card_ExtId(userId: Long): List<UserPlaylog>
 
     fun findByUser_Card_ExtId(userId: Long, page: Pageable): Page<UserPlaylog>
-
-    fun findByUser_Card_ExtIdAndMusicIdAndLevel(userId: Long, musicId: Int, level: Int): List<UserPlaylog>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserRivalDataRepository")
 interface UserRivalDataRepository : JpaRepository<UserRival, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserRival>
-
-    @Transactional
-    fun removeByUser_Card_ExtIdAndRivalUserExtId(userId: Long, rivalUserId: Long)
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserScenarioRepository")
@@ -250,9 +181,6 @@ interface UserScenarioRepository : JpaRepository<UserScenario, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserScenario>
 
     fun findByUserAndScenarioId(user: UserData, scenarioId: Int): Optional<UserScenario>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserStoryRepository")
@@ -260,9 +188,6 @@ interface UserStoryRepository : JpaRepository<UserStory, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserStory>
 
     fun findByUserAndStoryId(userData: UserData, storyId: Int): Optional<UserStory>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserTechCountRepository")
@@ -270,9 +195,6 @@ interface UserTechCountRepository : JpaRepository<UserTechCount, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserTechCount>
 
     fun findByUserAndLevelId(user: UserData, levelId: Int): Optional<UserTechCount>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserTechEventRepository")
@@ -280,9 +202,6 @@ interface UserTechEventRepository : JpaRepository<UserTechEvent, Long> {
     fun findByUser_Card_ExtId(userId: Long): List<UserTechEvent>
 
     fun findByUserAndEventId(userData: UserData, eventId: Int): Optional<UserTechEvent>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserTradeItemRepository")
@@ -300,9 +219,6 @@ interface UserTradeItemRepository : JpaRepository<UserTradeItem, Long> {
         chapterId: Int,
         tradeItemId: Int
     ): Optional<UserTradeItem>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 @Repository("OngekiUserTrainingRoomRepository")
@@ -310,9 +226,6 @@ interface UserTrainingRoomRepository : JpaRepository<UserTrainingRoom, Long> {
     fun findByUserAndRoomId(user: UserData, roomId: Int): Optional<UserTrainingRoom>
 
     fun findByUser_Card_ExtId(userId: Long): List<UserTrainingRoom>
-
-    @Transactional
-    fun deleteByUser(user: UserData)
 }
 
 
