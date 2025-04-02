d2983253bbede43bfe3ca0f295336f7b47fb300e
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2025-04-02 12:09:58 -0400

    [O] Communities

--- AquaNet/src/pages/Home/Communities.svelte
+++ AquaNet/src/pages/Home/Communities.svelte
@@ -9,15 +9,23 @@
 <div class="setup-instructions">
   <h2>{t('home.join-community')}</h2>
   <div class="grid cols-3 gap-4">
-    <CommunityCard color="82, 93, 233" icon="ic:baseline-discord" on:click={() => window.location.href = DISCORD_INVITE}>
-      <h3>Discord</h3>
-    </CommunityCard>
-    <CommunityCard color="46, 163, 224" icon="mingcute:telegram-fill" on:click={() => window.location.href = TELEGRAM_INVITE}>
-      <h3>Telegram</h3>
-    </CommunityCard>
-    <CommunityCard color="226, 60, 68" icon="ri:qq-fill" on:click={() => window.location.href = QQ_INVITE}>
-      <h3>QQ</h3>
-    </CommunityCard>
+    {#if DISCORD_INVITE}
+      <CommunityCard color="82, 93, 233" icon="ic:baseline-discord" on:click={() => window.location.href = DISCORD_INVITE}>
+        <h3>{t('home.community.discord')}</h3>
+      </CommunityCard>
+    {/if}
+
+    {#if TELEGRAM_INVITE}
+      <CommunityCard color="46, 163, 224" icon="mingcute:telegram-fill" on:click={() => window.location.href = TELEGRAM_INVITE}>
+        <h3>{t('home.community.telegram')}</h3>
+      </CommunityCard>
+    {/if}
+
+    {#if QQ_INVITE}
+      <CommunityCard color="226, 60, 68" icon="ri:qq-fill" on:click={() => window.location.href = QQ_INVITE}>
+        <h3>{t('home.community.qq')}</h3>
+      </CommunityCard>
+    {/if}
   </div>
 </div>
 
